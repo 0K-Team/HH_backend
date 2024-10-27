8ce5bd8573a96f58304522c5bc50953d98159306
GGod <46885632+GGodPL@users.noreply.github.com>
2024-10-27 22:07:26 +0100

    add accounts and simple authorization using OAuth2
    Co-authored-by: Q1000Q <Q1000Q@users.noreply.github.com>

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,13 @@ import routes from "./routes";
 import mongoose from "mongoose";
 import "dotenv/config";
 
+import passport from "passport";
+import "./auth/google";
+import "./auth/facebook";
+
+import session from "express-session";
+import MongoStore from "connect-mongo";
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URL as string)
@@ -12,6 +19,17 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use("/api", routes);
+
+app.use(passport.initialize());
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URL as string
+    }),
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000
+    }
+}))
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
