c3ff7dae12749e804e1950d851c51cb8bf190245
q1000q <1000q.mj@gmail.com>
2024-10-31 18:10:04 +0100

    Added endpoint for changing username

--- src/routes/user/user.ts
+++ src/routes/user/user.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import passport from "passport";
-import User from "../../schemas/accounts"
+import AccountData from "../../schemas/accounts"
+import { Aggregate } from "mongoose";
 
 const router = Router();
 
@@ -9,8 +10,8 @@ router.get("/me", passport.authenticate("jwt", { session: false }), (req, res) =
 })
 
 router.get("/:user", async (req, res) => {
-    const reqUser: String = req.params.user;
-    const userAggregate = User.aggregate([
+    const reqUser: String = await req.params.user;
+    const userAggregate = AccountData.aggregate([
         { $match: { username: reqUser } },
         { $project: { _id: 0, id: 1, username: 1, avatarHash: 1, title: 1, bio: 1, achievements: 1, skills: 1, badges: 1, location: 1, preferredTopics: 1 } }
     ]);
@@ -20,6 +21,22 @@ router.get("/:user", async (req, res) => {
         res.send(result);
     } catch (error) {
         res.status(500).send(error);
+    }
+})
+
+router.patch("/me/changeUsername/:newName", passport.authenticate("jwt", { session: false }), (req, res) => {
+    const newUsername: String = req.params.newName;
+    try {
+        const result = AccountData.findOneAndUpdate({
+            //@ts-ignore
+            id: req.user.id,
+        }, {
+            username: newUsername,
+        }).exec();
+        //@ts-ignore
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(500).send(err);
     }
 })
 
