c5cc9351f4c33c27a84ee3dd7380c37461dcf19b
GGod <46885632+GGodPL@users.noreply.github.com>
2024-11-05 15:33:38 +0100

    querying quizzes by category

--- src/routes/quizRouter.ts
+++ src/routes/quizRouter.ts
@@ -1,15 +1,23 @@
 import { Router } from "express";
 import QuizSchema from "../schemas/quizzes";
-import { validateParams } from "../middlewares/validate";
+import { validateParams, validateQuery } from "../middlewares/validate";
 import { ObjectIdValidator } from "../validators";
+import Joi from "joi";
 
 const router = Router();
 
-router.get("/", async (_, res) => {
+router.get("/", validateQuery(Joi.object({ category: Joi.string() })), async (req, res) => {
+    const { category } = req.query;
     try {
-        const result = await QuizSchema.aggregate([
-            { $project: { topic: 1, description: 1, category: 1, difficulty_level: 1, points_reward: 1 } }
-        ]).exec();
+        const result = await QuizSchema.find(category ? { 
+            category
+        } : {}, {
+            topic: 1, 
+            description: 1, 
+            category: 1, 
+            difficulty_level: 1,
+            points_reward: 1
+        });
         
         res.status(200).send(result);
     } catch (error) {
